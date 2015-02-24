712272768ec692c858ea32235f4de52dcdb107db
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2015-02-24 23:18:13 +0100

    Parametro double-optin configurabile su subscribe alla lista

--- CMailChimp.php
+++ CMailChimp.php
@@ -14,13 +14,10 @@
  * ===============================
  * 
  * @author Maurizio Cingolani <mauriziocingolani74@gmail.com>
- * @version 1.0
+ * @version 1.0.1
  */
 class CMailChimp extends MailChimp {
 
-    /**
-     * 
-     */
     public function __construct($api_key, $list_id) {
         parent::__construct($api_key, $list_id);
     }
@@ -29,7 +26,7 @@ class CMailChimp extends MailChimp {
         $lists = $this->call('lists/list', array());
         foreach ($lists['data'] as $list) ://Identifico la lista
             if ($list['id'] === $this->list_id)
-            break;
+                break;
         endforeach;
         return $list;
     }
@@ -41,13 +38,25 @@ class CMailChimp extends MailChimp {
         ));
     }
 
+    /**
+     * Aggiunge un nominativo alla lista.
+     * Parametri impostabili tramite l'oggetto passato come parametro:
+     * <ul>
+     * <li>fname: nome </li>
+     * <li>lname: cognome </li>
+     * <li>email: indirizzo email </li>
+     * <li>double_optin: true per richiedere iscrizione per email, false per iscrivere direttamente</li>
+     * </li>
+     * @param mixed $data Oggetto con i parametri
+     * @return type Array con i risultati, oppure false in caso di errore
+     */
     public function listsSubscribe($data) {
         return $this->call('lists/subscribe', array(
                     'id' => $this->list_id,
                     'email' => array(
                         'email' => $data->email,
                     ),
-                    'double_optin' => true,
+                    'double_optin' => $data->double_optin,
                     'merge_vars' => array(
                         'FNAME' => $data->fname,
                         'LNAME' => $data->lname,
